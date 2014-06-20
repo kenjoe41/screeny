9d9670679a7abc6fd7f7b9fdcca9821e6daf5ed6
Daniel Phillips <dpdk@me.com>
2014-06-21 01:01:15 +0200

    First push.. upload/delete remote file works.

---
+++ screeny.py
@@ -0,0 +1,37 @@
+################################################################################################
+# Name: Screeny.                                                                               #
+# Description: A tool to take screen shots and upload them to imgur from within the Terminal.  #
+# Author: Phage (phage@evilzone.org)                                                           #  
+################################################################################################
+
+import argparse
+import os
+import pyimgur
+
+import pyscreenshot as ImageGrab
+
+class Screeny(object):
+
+	def __init__(self, args):
+		self.args = args
+
+	def upload_to_imgur(self):
+		CLIENT_ID = "5c6af8a99249f98"
+
+		ImageGrab.grab_to_file(args.upload)
+		im = pyimgur.Imgur(CLIENT_ID)
+		uploaded_image = im.upload_image(args.upload, title="Uploaded with PyImgur")
+		print(uploaded_image.link)
+
+		try:
+			os.remove(args.upload)
+		except OSError, e:
+			print ("Error: %s - %s." % (e.filename,e.strerror))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(prog="Screeny")
+    parser.add_argument('-u', '--upload', help="upload the screenshot to imgur")
+
+    args = parser.parse_args()
+    screeny = Screeny(args)
+    screeny.upload_to_imgur()
