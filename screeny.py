e04ec7bb7e4324a5bf1effc382663afdb8004d13
Daniel Phillips <dpdk@me.com>
2014-06-21 13:56:44 +0200

    added a timer.

--- screeny.py
+++ screeny.py
@@ -7,6 +7,7 @@
 import argparse
 import os
 import pyimgur
+import time
 
 import pyscreenshot as ImageGrab
 
@@ -14,7 +15,7 @@ class Screeny(object):
 
 	def __init__(self, args):
 		self.args = args
-
+	
 	def upload_to_imgur(self):
 		CLIENT_ID = "5c6af8a99249f98"
 
@@ -31,7 +32,12 @@ class Screeny(object):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog="Screeny")
     parser.add_argument('-u', '--upload', help="upload the screenshot to imgur")
+    parser.add_argument('-t', '--timer', help="Sets a timer on the screenshot.", type=int, required=False)
 
     args = parser.parse_args()
+
+    if args.timer:
+    	time.sleep(args.timer)
+
     screeny = Screeny(args)
     screeny.upload_to_imgur()
